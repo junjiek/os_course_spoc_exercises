ad219216125f47d5001c18415389330fa3d73fb0
s0248 <kjj3300@163.com>
2016-03-07 11:11:54 +0800

    update

--- umalloc.c
+++ umalloc.c
@@ -19,28 +19,6 @@ typedef union header Header;
 static Header base;
 static Header *freep;
 
-void myfree(void *ap) {
-  Header *bp, *p;
-
-  bp = (Header*)ap - 1;
-  for (p = freep; !(bp > p && bp < p->s.ptr); p = p->s.ptr) {
-    if (p >= p->s.ptr && (bp > p || bp < p->s.ptr)) {
-      break;
-    }
-  }
-  if (bp + bp->s.size == p->s.ptr) {
-    bp->s.size += p->s.ptr->s.size;
-    bp->s.ptr = p->s.ptr->s.ptr;
-  } else
-    bp->s.ptr = p->s.ptr;
-  if (p + p->s.size == bp) {
-    p->s.size += bp->s.size;
-    p->s.ptr = bp->s.ptr;
-  } else
-    p->s.ptr = bp;
-  freep = p;
-}
-
 static Header* morecore(unsigned int nu) {
   char *p;
   Header *hp;
@@ -83,6 +61,28 @@ void* mymalloc(unsigned int nbytes) {
       }
     }
   }
+}
+
+void myfree(void *ap) {
+  Header *bp, *p;
+
+  bp = (Header*)ap - 1;
+  for (p = freep; !(bp > p && bp < p->s.ptr); p = p->s.ptr) {
+    if (p >= p->s.ptr && (bp > p || bp < p->s.ptr)) {
+      break;
+    }
+  }
+  if (bp + bp->s.size == p->s.ptr) {
+    bp->s.size += p->s.ptr->s.size;
+    bp->s.ptr = p->s.ptr->s.ptr;
+  } else
+    bp->s.ptr = p->s.ptr;
+  if (p + p->s.size == bp) {
+    p->s.size += bp->s.size;
+    p->s.ptr = bp->s.ptr;
+  } else
+    p->s.ptr = bp;
+  freep = p;
 }
 
 void memtest(void) {
